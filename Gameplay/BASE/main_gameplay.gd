f3885dc9c850c16b9bfce206ba51939d88821585
IsaacS2Hud <sanchezisaac809@gmail.com>
2024-12-29 16:17:07 -0600

    Adds unfinished scenes for rounds 1 and 2
    These scenes still need transitions between each other, which will be done by placing enemies in each scene in a group for enemies, and obtaining the amount of enemies at the start of each round. The enemies will have signals for death that the main_gameplay scene is listening to, decrementing the enemy count until it's zero to transition to the next round.

--- Gameplay/BASE/main_gameplay.gd
+++ Gameplay/BASE/main_gameplay.gd
@@ -9,8 +9,13 @@ var player: PlayerCharacter2D
 var player_spawn_point: Node2D
 var lives_left: int
 
+var enemy_count: int
+
 func accept_player_death() -> void:
 	_handle_player_death()
+
+func accept_enemy_death() -> void:
+	_handle_enemy_death()
 
 func _ready() -> void:
 	_init_game()
@@ -19,6 +24,7 @@ func _ready() -> void:
 func _init_game() -> void:
 	player = get_tree().get_first_node_in_group(Groups.player_character)
 	player_spawn_point = get_tree().get_first_node_in_group(Groups.player_spawn)
+	enemy_count = get_tree().get_nodes_in_group(Groups.enemy_character).size()
 	lives_left = STARTING_LIVES
 	player.global_position = player_spawn_point.global_position
 
@@ -29,9 +35,17 @@ func _handle_player_death() -> void:
 	else:
 		_run_game_over()
 
+func _handle_enemy_death() -> void:
+	enemy_count -= 1
+	if enemy_count > 0:
+		_run_next_round()
+
 func _respawn_player() -> void:
 	player.global_position = player_spawn_point.global_position
 
 func _run_game_over() -> void:
 	get_tree().paused = true
 	print("Game Over")
+
+func _run_next_round() -> void:
+	pass
