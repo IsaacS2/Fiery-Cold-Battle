2a0a3a5a1fb51a11955316c52d62b43682aec2e6
Thomas James Timusk <84927705+tjtimusk@users.noreply.github.com>
2024-12-29 14:54:25 -0500

    put in a bouncing sound effect

--- Code/Enums and Globals/sound_manager.gd
+++ Code/Enums and Globals/sound_manager.gd
@@ -1,5 +1,7 @@
 extends Node
 
+#region Test Tones
+
 func play_test_loop():
 	$test_tones/test_loop.play()
 
@@ -11,11 +13,13 @@ func play_test_saw():
 
 func play_test_square():
 	$test_tones/test_square.play()
+#endregion
 
 func play_music():
 	$music.play()
 
-
+func play_projectile_bounce():
+	$Projectiles/projectile_bounce.play()
 
 func _ready() -> void:
 	play_music()
